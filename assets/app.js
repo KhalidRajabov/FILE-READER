51e146f10971d4bcdc709312bc89d3e4940822b2
dbBurzum <khalidsr@code.edu.az>
2022-05-27 18:57:36 +0400

    update

--- assets/app.js
+++ assets/app.js
@@ -2,34 +2,40 @@ let upload = document.getElementById("upload")
 let input = document.getElementById("input")
 let table = document.getElementById("t-body")
 let btnRemove = document.getElementById("btn-remove")
+
 upload.onclick=_=>{input.click()}
 btnRemove.addEventListener("click",_=>{
     table.innerHTML="";
+    btnRemove.classList.add("hide")
 })
 input.addEventListener("change",e=>{
     for (const file of e.target.files) {
-        let reader = new FileReader;
-        reader.readAsDataURL(file)
-        let spantag = `<span class="span" style="cursor: pointer" id="span">x</span>`
-        reader.onloadend=function(image){
-            console.log(file.name + " file loaded");
-            let tr = `
-            <tr>
-            <th scope="row"><img src="${image.target.result}" width="100"></th>
-            <td>${file.name}</td>
-            <td>${file.size}</td>
-            <td>${spantag}</td>
-            </tr>
-            `
-            table.innerHTML+=tr
-            let span = document.querySelectorAll(".span")
-            span.forEach(item_=>{
-                item_.onclick=function(){
-                    item_.parentElement.parentElement.remove()
-                }
-            })
-            input.value=""
+            let reader = new FileReader;
+            reader.readAsDataURL(file)
+            let spantag = `<span class="span" style="cursor: pointer" id="span">x</span>`
+            reader.onloadend=function(image){
+                console.log(file.name + " file loaded");
+                let tr = `
+                <tr>
+                <th scope="row"><img src="${image.target.result}" width="100"></th>
+                <td>${file.name}</td>
+                <td>${file.size}</td>
+                <td>${spantag}</td>
+                </tr>
+                `
+                table.innerHTML+=tr
+                let span = document.querySelectorAll(".span")
+                span.forEach(item_=>{
+                    item_.onclick=function(){
+                        item_.parentElement.parentElement.remove()
+                    }
+                })
+                input.value=""
+                btnRemove.classList.remove("hide")
+            }
         }
     }
-}
-)
+    )
+    if (table.innerHTML=="") {
+        btnRemove.classList.add("hide")
+        }
